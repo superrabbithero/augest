32adf621ec97ed63208c2a3ccf99be30e5813fba
superrabbithero <huangding1996@gmail.com>
2024-06-30 21:40:17 +0800

    像素编辑器更新

--- src/components/pixelSvgEditor.vue
+++ src/components/pixelSvgEditor.vue
@@ -1,16 +1,26 @@
 <template>
   <input type="text" v-model="rows">x
   <input type="text" v-model="cols">
-  <canvas :width="width" :height="width" class="drawing-area" ref="canvas"
+  <div class="drawing-area">
+    <canvas :width="width" :height="height" ref="canvas"
               @pointerdown="handlePointerDown"
               @pointermove="handlePointerMove"
-              @pointerup="handlePointerUp"></canvas>
+              @pointerup="handlePointerUp"
+              @pointerleave="handlePointerLeave"
+              ></canvas>
+    <div class="rect" v-for="x in rows*cols" :style="{top:((x-1)%rows)*gridSize+'px',left:(Math.floor((x-1)/rows)*gridSize+'px'),position:'absolute', width:gridSize+'px',height:gridSize+'px',backgroundColor:(((x-1)%rows)+Math.floor((x-1)/rows))%2==0 ? '#fff' : '#eee'}">
+    </div>
+  </div>
+ 
   <div class="tools-bar">
     <button @click="tool = 1">画笔</button>
     <button @click="tool = 2">橡皮</button>
     <button @click="tool = 3">直线</button>
     <button @click="tool = 4">矩形</button>
     <button @click="tool = 5">圆形</button>
+    <button @click="filled = !filled">{{filled ? "填充" : "不填充"}}</button>
+    <button @click="tool = 6">填充</button>
+    <button @click="clearAll">删除</button>
   </div>
 </template>
 
@@ -29,13 +39,14 @@ export default {
       tool:1,
       rows:30,
       cols:30,
-      gridSize:20,
+      gridSize:10,
       currentColor:'#252525',
       isDrawing: false,
       pixels: [],
       endPoints:{x:0,y:0},
       ctx:null,
-      historys:[]
+      historys:[],
+      filled:false,
     };
   },
   mounted() {
@@ -56,24 +67,47 @@ export default {
       this.endPoints = this.getPoint({x:event.clientX,y:event.clientY})
       if(this.tool == 1) this.drawPixel(this.endPoints)
       else if(this.tool == 2) this.clearPixel(this.endPoints)
+      else if(this.tool == 6){
+        this.isDrawing = false
+        console.log(this.tool)
+        this.ctx.fill()
+      }
       
     },
     handlePointerMove(event){
-      const end = this.getPoint({x:event.clientX,y:event.clientY})
+      const currPoint = this.getPoint({x:event.clientX,y:event.clientY})
       if(this.isDrawing){
         if(this.tool == 1){
-          this.drawPixel(end)
+          this.drawPixel(currPoint)
         }else if(this.tool == 2){
-          this.clearPixel(end)
+          this.clearPixel(currPoint)
         }else if(this.tool == 3){
           this.showLastHistory()
-          this.drawLine(this.endPoints, end)
+          this.drawLine(this.endPoints, currPoint)
+        }else if(this.tool == 4){
+          this.showLastHistory()
+          this.drawRect(this.endPoints, currPoint)
+        }else if(this.tool == 5){
+          this.showLastHistory()
+          this.drawCircle(this.endPoints, currPoint);
+        }
+      }else{
+        if(true){
+          this.showLastHistory()
+          this.drawPixel(currPoint)
         }
       }
     },
     handlePointerUp(){
-      this.isDrawing = false
-      this.addHistoy()
+      if(this.isDrawing){
+        this.isDrawing = false
+        this.addHistoy()
+      }
+      
+    },
+    handlePointerLeave(){
+      if(this.isDrawing == false)
+        this.showLastHistory()
     },
     drawPixel(point){
       this.ctx.fillRect(point.x,point.y,this.gridSize,this.gridSize)
@@ -103,6 +137,49 @@ export default {
         }
       }
     },
+    drawRect(start,end){
+      
+      if(this.filled){
+        this.ctx.fillRect(start.x,start.y,end.x-start.x+this.gridSize,end.y-start.y+this.gridSize)
+      }else{
+        this.ctx.lineWidth = this.gridSize
+        this.ctx.strokeRect(start.x+this.gridSize/2,start.y+this.gridSize/2,end.x-start.x,end.y-start.y)
+      }
+    },
+    drawCircle(start, end) {
+      const radius = Math.sqrt(Math.pow((end.x - start.x) / this.gridSize, 2) + Math.pow((end.y - start.y) / this.gridSize, 2));
+      const centerX = Math.floor(start.x / this.gridSize);
+      const centerY = Math.floor(start.y / this.gridSize);
+
+      let x = Math.floor(radius);
+      let y = 0;
+      let decisionOver2 = 1 - x;
+
+      while (x >= y) {
+        this.drawCirclePoints(centerX, centerY, x, y);
+        y++;
+        if (decisionOver2 <= 0) {
+          decisionOver2 += 2 * y + 1;
+        } else {
+          x--;
+          decisionOver2 += 2 * (y - x) + 1;
+        }
+      }
+    },
+    drawCirclePoints(cx, cy, x, y) {
+      this.drawPixel({ x: (cx + x) * this.gridSize, y: (cy + y) * this.gridSize });
+      this.drawPixel({ x: (cx - x) * this.gridSize, y: (cy + y) * this.gridSize });
+      this.drawPixel({ x: (cx + x) * this.gridSize, y: (cy - y) * this.gridSize });
+      this.drawPixel({ x: (cx - x) * this.gridSize, y: (cy - y) * this.gridSize });
+      this.drawPixel({ x: (cx + y) * this.gridSize, y: (cy + x) * this.gridSize });
+      this.drawPixel({ x: (cx - y) * this.gridSize, y: (cy + x) * this.gridSize });
+      this.drawPixel({ x: (cx + y) * this.gridSize, y: (cy - x) * this.gridSize });
+      this.drawPixel({ x: (cx - y) * this.gridSize, y: (cy - x) * this.gridSize });
+    },
+    clearAll(){
+      this.ctx.clearRect(0,0,this.width,this.height)
+      this.addHistoy()
+    },
     getPoint(point){
       const rect = this.$refs.canvas.getBoundingClientRect();
       const x = Math.floor((point.x - rect.left) / this.gridSize)*this.gridSize
@@ -124,8 +201,20 @@ export default {
 
 <style>
 .drawing-area {
-  border: 1px solid #000;
-  display: block;
+  border: 0.5px solid #000;
+  position: relative;
+  width: fit-content;
+  font-size: 0;
   margin: 0 auto;
 }
+canvas {
+  position: relative;
+  z-index: 1;
+  touch-action: none;
+}
+.rect {
+  box-sizing: border-box;
+  pointer-events: none;
+}
+
 </style>
